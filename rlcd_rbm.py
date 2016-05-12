bf28304595c8208ebcbe301414583ced5037f68e
Di Sun <steven.d.sun@gmail.com>
2016-05-12 00:13:20 -0700

    algorithm working, result dump

--- rlcd_rbm.py
+++ rlcd_rbm.py
@@ -35,6 +35,9 @@ b = tf.Variable(tf.random_uniform([size_h, 1], -0.05, 0.05))
 W = tf.Variable(tf.random_uniform([size_x, size_h], -0.05, 0.05))
 c = tf.Variable(tf.random_uniform([size_x, 1], -0.05, 0.05))
 
+X1 = tf.Variable(tf.zeros([size_x, size_bt], tf.float32))
+H1 = tf.Variable(tf.zeros([size_h, size_bt], tf.float32))
+
 a = tf.placeholder(tf.float32)
 coldness = tf.placeholder(tf.float32) # coldness is 1/Temperature
 
@@ -44,10 +47,10 @@ sc = tf.placeholder(tf.float32, [1, size_bt]) # place holder for returned score
 # the const number for this
 norm_const = tf.Variable([20.00])
 
-an_step = tf.constant(0.2)
+an_step = tf.constant(0.01)
 
-x = sample(tf.ones([size_x, size_bt]) * 0.5)
-h = sample(tf.sigmoid(tf.matmul(tf.transpose(W)*0, x) + tf.tile(b*0, [1, size_bt])))
+x0 = sample(tf.ones([size_x, size_bt]) * 0.5)
+h0 = sample(tf.sigmoid(tf.matmul(tf.transpose(W)*0, x0) + tf.tile(b*0, [1, size_bt])))
 
 def simAnnealingGibbs(xx, hh, temp_inv):
     xk = sample(tf.sigmoid(tf.matmul(W*temp_inv, hh) + tf.tile(c*temp_inv, [1, size_bt])))
@@ -57,32 +60,30 @@ def simAnnealingGibbs(xx, hh, temp_inv):
 def isColdEnough(xx, hh, temp_inv):
     return temp_inv < 1.0
 
-[x1, h1, _] = control_flow_ops.While(isColdEnough, simAnnealingGibbs, [x, h, coldness], 1, False)
-x2 = sample(tf.sigmoid(tf.matmul(W, h1) + tf.tile(c, [1, size_bt])))
-h2 = sample(tf.sigmoid(tf.matmul(tf.transpose(W), x2) + tf.tile(b, [1, size_bt])))
-
-# def logProbXcondH(x, h, W, c):
-#     return tf.reduce_sum(tf.mul(tf.matmul(W, h) + tf.tile(c, [1, tf.shape(h)[1]]), scalePosNeg(x)), 0)
-
+[x1, h1, _] = control_flow_ops.While(isColdEnough, simAnnealingGibbs, [x0, h0, coldness], 1, False)
+sample_data = [X1.assign(x1), H1.assign(h1)]
 
 def logMargX(x, h, W, c):
     prob_all1 = tf.matmul(W, h) + tf.tile(c, [1, size_bt])
     # log_matrix has the DIM[0] the position of batch in h and DIM[1] the position of batch in x
     log_matrix = tf.reduce_sum(tf.tile(tf.expand_dims(prob_all1, -1), [1, 1, size_bt]) * \
         tf.transpose(tf.tile(tf.expand_dims(scalePosNegOne(x), -1), [1, 1, size_bt]), [0, 2, 1]), 1)
-    return tf.reduce_mean(tf.sigmoid(log_matrix), 0, True)
-    # TODO check how to make this work
+    return tf.log(tf.reduce_mean(tf.sigmoid(log_matrix), 0, True))
 
-updt_value = sc - logMargX(x1, h1, W, c) - tf.tile(tf.expand_dims(norm_const, -1), [1, size_bt])
-update_value = tf.minimum(tf.maximum(updt_value, 0), 10)
-update_value_norm = tf.minimum(tf.maximum(updt_value, -1), 10)
 # define the update rule
+updt_value = sc - logMargX(X1, H1, W, c) - tf.tile(tf.expand_dims(norm_const, -1), [1, size_bt])
+update_value = tf.minimum(tf.maximum(tf.exp(updt_value) - 1, -1), 10)
+update_value_norm = tf.minimum(tf.maximum(updt_value, -1), 10)
 
-W_ = a/float(size_bt) * tf.reduce_mean(tf.sub(tf.batch_matmul(tf.expand_dims(tf.transpose(x1), 2), tf.expand_dims(tf.transpose(h1), 1)),\
-     tf.batch_matmul(tf.expand_dims(tf.transpose(x2), 2), tf.expand_dims(tf.transpose(h2), 1))) * tf.tile(tf.reshape(update_value, [size_bt, 1, 1]), [1, size_x, size_h]), 0)
-b_ = a * tf.reduce_mean(tf.mul(tf.sub(h1, h2), tf.tile(update_value, [size_h, 1])), 1, True)
-c_ = a * tf.reduce_mean(tf.mul(tf.sub(x1, x2), tf.tile(update_value, [size_x, 1])), 1, True)
 norm_const_ = tf.mul(tf.reduce_mean(update_value_norm, 1), 2*a)
+
+x2 = sample(tf.sigmoid(tf.matmul(W, H1) + tf.tile(c, [1, size_bt])))
+h2 = sample(tf.sigmoid(tf.matmul(tf.transpose(W), x2) + tf.tile(b, [1, size_bt])))
+
+W_ = a/float(size_bt) * tf.reduce_mean(tf.sub(tf.batch_matmul(tf.expand_dims(tf.transpose(X1), 2), tf.expand_dims(tf.transpose(H1), 1)),\
+     tf.batch_matmul(tf.expand_dims(tf.transpose(x2), 2), tf.expand_dims(tf.transpose(h2), 1))) * tf.tile(tf.reshape(update_value, [size_bt, 1, 1]), [1, size_x, size_h]), 0)
+b_ = a * tf.reduce_mean(tf.mul(tf.sub(H1, h2), tf.tile(update_value, [size_h, 1])), 1, True)
+c_ = a * tf.reduce_mean(tf.mul(tf.sub(X1, x2), tf.tile(update_value, [size_x, 1])), 1, True)
 
 # norm_const
 # TODO not done, add the self engergy function estimation
@@ -97,10 +98,11 @@ sess.run(init)
 norm_hist = []
 sample_score_hist = []
 # loop with batch
-for i in range(1, 30002):
-    alpha = min(0.05, 500.0/float(i))
-    x1_ = sess.run(x1, feed_dict={coldness: 0.00})
-    score = 20.0 * muscleTorqueScore(size_x, side_x, x1_)
+for i in range(1, 10002):
+    alpha = min(0.05, 10.0/float(i))
+    sess.run(sample_data, feed_dict={coldness: 0.0})
+    x1_ = sess.run(X1)
+    score = 10.0 * muscleTorqueScore(size_x, side_x, x1_)
     sample_score_hist.extend(score.tolist())
     # print sample_score_hist
     # print "shape of W_", sess.run(tf.shape(W_), feed_dict={ sc: score, a: alpha, coldness: 0.0}), sess.run(tf.shape(W))
@@ -109,28 +111,33 @@ for i in range(1, 30002):
     # print "shape of norm_const_", sess.run(tf.shape(norm_const_), feed_dict={ sc: score, a: alpha, coldness: 0.0}), sess.run(tf.shape(norm_const))
     # print "logMargX", sess.run(logMargX(x1, h1, W, c), feed_dict={ sc: score, a: alpha, coldness: 0.00})
     # print "shape of updt_value", sess.run(tf.shape(updt_value), feed_dict={ sc: score, a: alpha, coldness: 0.0}), "value ", sess.run(updt_value, feed_dict={ sc: score, a: alpha, coldness: 0.0})
-    sess.run(updt, feed_dict={ sc: score, a: alpha, coldness: 0.0})
+    sess.run(updt, feed_dict={ sc: score, a: alpha})
     print i, ' mean score ', np.mean(score), ' max score ', np.max(score), ' step size ', alpha, ' norm_const ', sess.run(norm_const)
     # vidualization
-    if i % 1000 == 1:
+    if i % 500 == 1:
         image = Image.fromarray(tile_raster_images(sess.run(W).T,
                                                    img_shape=(side_x, side_x),
                                                    tile_shape=(side_h, side_h),
                                                    tile_spacing=(2, 2)))
         image.show()
-        image = Image.fromarray(tile_raster_images(sess.run(x1, feed_dict={ sc: score, a: alpha, coldness: 0.0}).T,
+        image = Image.fromarray(tile_raster_images(sess.run(X1).T,
                                                    img_shape=(side_x, side_x),
                                                    tile_shape=(side_h, side_h),
                                                    tile_spacing=(2, 2)))
         image.show()
+        image = Image.fromarray(tile_raster_images(sess.run(c).T,
+                                                   img_shape=(side_x, side_x),
+                                                   tile_shape=(1, 1),
+                                                   tile_spacing=(2, 2)))
+        image.show()
         print 'norm_const ', sess.run(norm_const).T
         # plt.plot(sample_score_hist)
-        plt.show() 
         # print 'c ', sess.run(c).T
         # print 'b ', sess.run(b).T
         # print 'W ', sess.run(W).T
         
 plt.plot(sample_score_hist)
+plt.show()
 
 
 
